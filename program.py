a1974cf6f9c694189b04abc0ab08463c1f6ea1c8
Евгений Минеев <ew.mineew@ya.ru>
2022-09-30 00:49:54 +0800

    Спринт 2. Модуль фитнес-трекера. Версия 1

--- program.py
+++ program.py
@@ -1,1 +1,17 @@
-print('Я домашка'
+from math import sqrt
+
+
+message = ['Добро пожаловать в самую лучшую программу для вычисления',
+          'квадратного корня из заданного числа']
+
+def  Calculate_square_root(number):
+    """Вычисляет квадратный корень."""
+    return sqrt(number)
+
+def calc(your_number) :
+    if your_number <= 0:
+        return    
+     
+    root = 0
+    answer = Calculate_square_root(your_number)
+    print(f'Мы вычислили корень квадратный из введенного вами числа. Это будет: {answer}')
